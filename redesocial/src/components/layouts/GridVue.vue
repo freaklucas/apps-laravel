7e00ab254c16848df64f46617cbe0c3709991e81
freaklucas <lucas.oliveira.freak@gmail.com>
2020-12-16 16:41:30 -0300

    grid com componente

---
+++ redesocial/src/components/layouts/GridVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="'column is-' + tamanho">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'GridVue',
+    props:['tamanho'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
